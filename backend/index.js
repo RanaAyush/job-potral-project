3fbe931a9f2270c5ea6574635db9daa081601707
Ayush Rana <ashusinghrana1038@gmail.com>
2024-10-08 08:10:53 +0530

    created job potral

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,20 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+// import { urlencoded } from "body-parser";
 
 dotenv.config({});
-
+connectDB();
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+// app.use(urlencoded({extended:true}))
 app.use(cookieParser());
 const corsOptions = {
     origin:'http://localhost:5173',
@@ -23,18 +29,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
-
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+})
 
 app.listen(PORT,()=>{
-    connectDB();
     console.log(`Server running at port ${PORT}`);
 })
